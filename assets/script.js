d95b0a0a42d4aa374ed6d7a884ab9a38c0d8f1d3
PacDraft <Kirsten@PacificDraftWorks.com>
2021-10-31 14:34:31 -0700

    added comments to index

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
- //Quiz Questions
+ //Quiz Questions & answers and correct answer
  const questions = [
   {
       question: "Inside which HTML element do we put the JavaScript?",
@@ -22,24 +22,24 @@
   },
   {
       question: "How do you add a comment in a JavaScript?",
-      choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
-      answer: "a. //This is a comment"
+      choices: ["a. 'This is a comment", "b. <!--This is a comment-->", "c. //This is a comment", "d. * This is a comment *"],
+      answer: "c. //This is a comment"
   },
   {
       question: "Which event occurs when the user clicks on an HTML element?",
-      choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
-      answer: "a. onclick"
+      choices: ["a. onmouseclick", "b. onchange", "c. onmouseover", "d. onclick"],
+      answer: "d. onclick"
   }
 ];
 
-//Timer elements
+//Timer variables
 var timer = document.getElementById("timer");
 var timeRemaining = document.getElementById("timeRemaining");
 var timesUp = document.getElementById("timesUp");
-//Start button elements
+//Start button variables
 var startDiv = document.getElementById("start");
 var startQuizBtn = document.getElementById("start-quiz-button");
-//Question elements
+//Question variables
 var questionDiv = document.getElementById("questionDiv");
 var questionTitle = document.getElementById("questionTitle");
 //Answer variables
@@ -49,29 +49,28 @@ var choiceC = document.getElementById("btn2");
 var choiceD = document.getElementById("btn3");
 var answerCheck = document.getElementById("answerCheck");
 var summary = document.getElementById("summary");
-
+//Initials variables-for scorekeeping
 var submitInitialBtn = document.getElementById("submitInitialBtn");
 var initialInput = document.getElementById("initialInput");
 var quizBox = document.getElementById("quiz");
-//High Scores
+//High Scores variables
 var highScoreSection = document.getElementById("highScoreSection");
 var finalScore = document.getElementById("finalScore");
 var goBackBtn = document.getElementById("goBackBtn");
 var clearHighScoreBtn = document.getElementById("clearHighScoreBtn"); 
 var viewHighScore = document.getElementById("viewHighScore");
 var listOfHighScores = document.getElementById("listOfHighScores");
-
-// define other variables
+//Additional variables for answers & questions
 var correctAns = 0;
 var questionNum = 0;
 var scoreResult;
 var questionIndex = 0;
 
-//Timer, begins when start button is clicked
-var totalTime = 101;
+//when quiz begins, start timer
+var totalTime = 76;
 function startQuiz() {
   questionIndex = 0;
-  totalTime = 100;
+  totalTime = 75;
   timeRemaining.textContent = totalTime;
   initialInput.textContent = "";
 
@@ -86,7 +85,7 @@ function startQuiz() {
       if(totalTime <= 0) {
           clearInterval(startTimer);
           if (questionIndex < questions.length - 1) {
-              gameOver();
+              quizEnd();
           }
       }
   },1000);
@@ -94,7 +93,7 @@ function startQuiz() {
   showQuiz();
 };
 
-//User is presented with questions and answer choices
+//Functions to display questions on the quiz
 function showQuiz() {
   nextQuestion();
 }
@@ -107,7 +106,7 @@ function nextQuestion() {
   choiceD.textContent = questions[questionIndex].choices[3];
 }
 
-//After the user answers the question, show whether their answer is correct or incorrect
+//function to check answer
 function checkAnswer(answer) {
 
   var lineBreak = document.getElementById("lineBreak");
@@ -117,22 +116,23 @@ function checkAnswer(answer) {
   if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
       //If the answer is correct, add 1 to the final score
       correctAns++;
-      // console.log(correctAns);
+      //Display to user that their answer was correct
       answerCheck.textContent = "Correct!";
   } else {
-      // wrong answer, deduct 10 seconds from timer
+      //If answer was incoorect, subtract 10 seconds from the timer
       totalTime -= 10;
       timeRemaining.textContent = totalTime;
+      //Display to user that their answer was incorrect and show the correct answer
       answerCheck.textContent = "Incorrect! The correct answer is: " + questions[questionIndex].answer;
   }
 
   questionIndex++;
-  //Repeat the quiz with the remaining questions 
+  //Continue quiz with additional questions
   if (questionIndex < questions.length) {
       nextQuestion();
   } else {
-      //If all of the questions have been completed, end the sequence
-      gameOver();
+      //If all of the questions have been answered, finish the quiz
+      quizEnd();
   }
 }
 
@@ -145,22 +145,20 @@ function chooseC() { checkAnswer(2); }
 function chooseD() { checkAnswer(3); }
 
 //Once all questions have been answered or when the timer reaches 0, quiz ends
-function gameOver() {
+function quizEnd() {
   summary.style.display = "block";
   questionDiv.style.display = "none";
   startDiv.style.display = "none";
   timer.style.display = "none";
   timesUp.style.display = "block";
-
-  // show final score
   finalScore.textContent = correctAns;
 }
 
-//User enters initials and high score is recorded in local storage
+//function to display high score
 function storeHighScores(event) {
   event.preventDefault();
 
-  //If the user does not enter their initials alert them to please enter
+  //If the user does not enter anything in the intials box, prompt them to enter a value
   if (initialInput.value === "") {
       alert("Please enter your initials!");
       return;
@@ -172,7 +170,7 @@ function storeHighScores(event) {
   summary.style.display = "none";
   highScoreSection.style.display = "block";   
 
-  //Record scores into local storage
+  //variables to record and pull scores from local storage
   var savedHighScores = localStorage.getItem("high scores");
   var scoresArray;
 
@@ -181,12 +179,12 @@ function storeHighScores(event) {
   } else {
       scoresArray = JSON.parse(savedHighScores)
   }
-
+  //calculate the users score and display with initials
   var userScore = {
       initials: initialInput.value,
       score: finalScore.textContent
   };
-
+  //console log the score
   console.log(userScore);
   scoresArray.push(userScore);
 
@@ -194,11 +192,11 @@ function storeHighScores(event) {
   var scoresArrayString = JSON.stringify(scoresArray);
   window.localStorage.setItem("high scores", scoresArrayString);
   
-  //Show current high scores
+  //call function to show high scores
   showHighScores();
 }
 
-//function to show current high scores
+//display high scores
 var i = 0;
 function showHighScores() {
 
@@ -208,10 +206,9 @@ function showHighScores() {
   timesUp.style.display = "none";
   summary.style.display = "none";
   highScoreSection.style.display = "block";
-
   var savedHighScores = localStorage.getItem("high scores");
 
-  //check to see if there are any high scores saved in the local storage
+  //check to see if there are any high scores already saved in the local storage
   if (savedHighScores === null) {
       return;
   }
@@ -226,7 +223,7 @@ function showHighScores() {
   }
 }
 
-//Event listeners for buttons
+//event listeners for answer selections
 
 startQuizBtn.addEventListener("click", startQuiz);
 choiceA.addEventListener("click", chooseA);
@@ -234,19 +231,23 @@ choiceB.addEventListener("click", chooseB);
 choiceC.addEventListener("click", chooseC);
 choiceD.addEventListener("click", chooseD);
 
+//event listener and function to submit initials on high scores screen
 submitInitialBtn.addEventListener("click", function(event){ 
   storeHighScores(event);
 });
 
+//event listener and function to view high scores
 viewHighScore.addEventListener("click", function(event) { 
   showHighScores(event);
 });
 
+//event listener & fucntion for go back button on high scores menu
 goBackBtn.addEventListener("click", function() {
   startDiv.style.display = "block";
   highScoreSection.style.display = "none";
 });
 
+//event listener & fucntion to clear high scores
 clearHighScoreBtn.addEventListener("click", function(){
   window.localStorage.removeItem("high scores");
   listOfHighScores.innerHTML = "High Scores Cleared!";
